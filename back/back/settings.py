5f1dd9396d9afb95a9642704f7a37906f36e6949
Ali Rezaei <alirezaeim1380@gmail.com>
2023-12-30 18:22:34 +0330

    feat(back): Add api app

--- back/back/settings.py
+++ back/back/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'api',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'back.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'back_db',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'back-db',
+        'PORT': 8000,
     }
 }
 
